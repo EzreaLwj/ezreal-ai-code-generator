0e9185ea51e1058a0bb7a2ee56457af1ca1d0464
ezreal <956371950@qq.com>
2025-08-10 01:35:51 +0800

    feat: stream流式实现

--- src/main/java/com/ezreal/ai/code/generator/domain/ai/service/AiCodeGeneratorService.java
+++ src/main/java/com/ezreal/ai/code/generator/domain/ai/service/AiCodeGeneratorService.java
@@ -3,6 +3,7 @@ package com.ezreal.ai.code.generator.domain.ai.service;
 import com.ezreal.ai.code.generator.domain.ai.model.HtmlCodeResult;
 import com.ezreal.ai.code.generator.domain.ai.model.MultiFileCodeResult;
 import dev.langchain4j.service.SystemMessage;
+import reactor.core.publisher.Flux;
 
 /**
  * @author Ezreal
@@ -10,11 +11,15 @@ import dev.langchain4j.service.SystemMessage;
  */
 public interface AiCodeGeneratorService {
 
-    String generateCode(String userMessage);
-
     @SystemMessage(fromResource = "prompt/codegen-html-system-prompt.txt")
     HtmlCodeResult generateHtmlCode(String userMessage);
 
     @SystemMessage(fromResource = "prompt/codegen-multi-file-system-prompt.txt")
     MultiFileCodeResult generateMultiFileCode(String userMessage);
+
+    @SystemMessage(fromResource = "prompt/codegen-html-system-prompt.txt")
+    Flux<String> generateHtmlCodeStream(String userMessage);
+
+    @SystemMessage(fromResource = "prompt/codegen-multi-file-system-prompt.txt")
+    Flux<String> generateMultiFileCodeStream(String userMessage);
 }
